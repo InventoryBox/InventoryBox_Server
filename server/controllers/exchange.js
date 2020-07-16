e877755e54a166b245c13e6ba01f98dd6f011000
jungwook <neity16@daum.net>
2020-07-17 04:56:21 +0900

    2020-07-17 exchange home disDiff change

--- server/controllers/exchange.js
+++ server/controllers/exchange.js
@@ -92,7 +92,7 @@ const exchange = {
             return res.status(statusCode.BAD_REQUEST)
                 .send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_POSTS));
 
-        postList_re = [];
+        var postList_re = [];
         if (filter == 1) postList.sort((a, b) => (getDistance(a.latitude, a.longitude, userLoc[0].latitude, userLoc[0].longitude) > getDistance(b.latitude, b.longitude, userLoc[0].latitude, userLoc[0].longitude)) ? 1 : -1)
         for (var i = 0; i < postList.length; i++) {
             var dist = getDistance(postList[i].latitude, postList[i].longitude, userLoc[0].latitude, userLoc[0].longitude);
