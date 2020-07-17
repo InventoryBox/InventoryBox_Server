743216401cffd0e2a4a2e317b375332548fb598f
100sun <sunhyebaek99@gmail.com>
2020-07-17 11:07:44 +0900

    2020-07-17 default switch UPDATE

--- server/controllers/exchange.js
+++ server/controllers/exchange.js
@@ -69,8 +69,11 @@ const exchange = {
     // exchange/:filter 
     home: async (req, res) => {
         const userIdx = req.idx;
+        if (!userIdx) {
+            res.status(statusCode.BAD_REQUEST)
+                .send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        }
         const filter = req.params.filter;
-
         var filterStr;
         switch (parseInt(filter)) {
             case 0:
@@ -81,6 +84,9 @@ const exchange = {
                 break;
             case 2:
                 filterStr = "price ASC";
+                break;
+            default:
+                filterStr = "uploadDate DESC";
                 break;
         }
 
@@ -231,6 +237,9 @@ const exchange = {
                 break;
             case 2:
                 filterStr = "price ASC";
+                break;
+            default:
+                filterStr = "uploadDate DESC";
                 break;
         }
 
