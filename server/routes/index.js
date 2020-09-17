f63405bbb9b652e40e2b079d3b6a5c58dfb0fc9d
jungwook <neity16@daum.net>
2020-09-18 00:34:09 +0900

    2020-09-18 http->https 설정6

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index', function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Express'
     });
+});
+
+router.get('/test', function (req, res, next) {
+    console.log('test');
 });
 
 router.use('/auth', require('./auth'));
