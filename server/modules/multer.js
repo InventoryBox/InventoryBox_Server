dc6580ae4e23494cbd781b221a75477f1ccd6496
jungwook <neity16@daum.net>
2020-09-21 02:14:41 +0900

    2020-09-21 multer modify

--- server/modules/multer.js
+++ server/modules/multer.js
@@ -7,7 +7,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'sopt-26-ims',
+        bucket: 'inventorybox',
         acl: 'public-read',
         key: function(req, file, cb){
                 cb(null, Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
