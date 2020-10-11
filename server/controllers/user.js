99fb39f1abc1f2fc91abf83d6885be99e3e03e08
camel-man-ims <gudwnsrh@gmail.com>
2020-10-11 11:48:51 +0900

    20-10-11 auth/user/post 추가

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -523,3 +523,41 @@ exports.getUserPost = async(req,res)=>{
         result:result
     }))
 }
+
+exports.getUserPostIos = async(req,res)=>{
+
+    Date.prototype.hhmmss = function() {
+        var hh = this.getHours().toString();
+        var mm = this.getMinutes().toString();
+        var ss = this.getSeconds().toString();
+        return (hh[1] ? hh : "0" + hh[0]) + ":" + (mm[1] ? mm : "0" + mm[0]) + ":" + (ss[1] ? ss : "0" + ss[0]);
+    }
+
+    function dateToDotString(DateFunction) {
+        var month = (DateFunction.getMonth() + 1) < 10 ? '0' + (DateFunction.getMonth() + 1) : (DateFunction.getMonth() + 1);
+        var date = DateFunction.getDate() < 10 ? '0' + DateFunction.getDate() : DateFunction.getDate();
+        var time = DateFunction.hhmmss()
+        return DateFunction.getFullYear() + '.' + month + '.' + date + ' '+ time;
+    }
+
+    const userIdx = req.idx
+
+    if (userIdx === null) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMsg.AUTH_USER_IDX_NULL))
+    }
+
+    const result = await User.getUserPost(userIdx)
+
+    for(i in result){
+        var uploadDate = dateToDotString(result[i].uploadDate);
+        result[i].uploadDate = uploadDate;
+    }
+
+    if (result === null) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMsg.DB_ERROR))
+    }
+
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMsg.AUTH_GET_USER_POST_SUCCESS, {
+        result:result
+    }))
+}
